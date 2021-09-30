650a5e15fc0560743bf047347c09aaef569bb212
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-09-30 20:33:03 +0530

    secret keyword, findByPk

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,21 +1,21 @@
-const user = require("../models/user");
-const { secret } = require("../config/index");
+const User = require("../models/user");
+const { SECRET } = require("../config");
 const { Strategy, ExtractJwt } = require("passport-jwt");
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: secret,
+  secretOrKey: SECRET,
 };
+
 module.exports = (passport) => {
   passport.use(
     new Strategy(opts, async (payload, done) => {
-      await user
-        .findByPk(payload.user_id)
+      await User.findByPk(payload.user_id)
         .then((user) => {
           if (user) {
             return done(null, user);
           }
-          // return done(null, false);
+          return done(null, false);
         })
         .catch((err) => {
           return done(null, false);
