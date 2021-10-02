8783747315d66fadf1cc7dea9586d11cab7ca7ae
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-10-02 13:30:08 +0530

    sql injection handling - pg pool

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,15 @@ const pool = require("../database/onlyPostgres");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
+  /** Wrong way of including clauses and attributes. Prone to SQL injection.
+   *Avoid string concatenation
+   *  pool.query('Select * from "User" where id={req.query.id}', (err, result) => {}
+   *
+   *
+   *To handle sql injection,
+   * pool.query('Select * from "User" where id=$1 and emai$2',[req.query.id, req.query.email] ,(err, result) => {}
+   *
+   */
   pool.query('Select * from "User"', (err, result) => {
     if (err) {
       throw err;
